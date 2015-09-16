1c2572b3007e28616def3b9a5837d1b1dff756e3
Manisha Yeramareddy <manisha.yeramareddy@cashcall.com>
2015-09-15 18:44:08 -0700

    Initial Commit

---
+++ Cortex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cortex
+//
+//  Created by Manisha Yeramareddy on 9/15/15.
+//  Copyright (c) 2015 Manisha Yeramareddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
