c899c049441023cd5e163aef0d8b11252d07a25e
Manisha Yeramareddy <manisha.yeramareddy@cashcall.com>
2016-04-06 08:54:55 -0700

    #6

--- Cortex/CortexTabBarController.swift
+++ Cortex/CortexTabBarController.swift
@@ -28,21 +28,21 @@ class CortexTabBarController: UITabBarController {
                 if(i == 0) {
                     if let finalImage0 = UIImage(named:"PersonThoughtFilled.png") {
                         finalImage = finalImage0
+                        item.selectedImage = finalImage.imageWithColor(UIColor(rgba: "#DB3C41")).imageWithRenderingMode(.AlwaysOriginal)
                     }
                 } else if(i == 1) {
                     if let finalImage0 = UIImage(named: "FilingCabinetFilled.png") {
                         finalImage = finalImage0
+                        item.selectedImage = finalImage.imageWithColor(UIColor(rgba: "#3CB3B5")).imageWithRenderingMode(.AlwaysOriginal)
                     }
                 } else if(i == 2) {
                     if let finalImage0 = UIImage(named: "TrendsFilled.png") {
                         finalImage = finalImage0
+                        item.selectedImage = finalImage.imageWithColor(UIColor(rgba: "#35AE4A")).imageWithRenderingMode(.AlwaysOriginal)
                     }
-                } else if(i == 3) {
-                    if let finalImage0 = UIImage(named: "SettingsFilled.png") {
-                        finalImage = finalImage0
-                    }
+                } else {
+                    item.selectedImage = finalImage.imageWithColor(UIColor(rgba:"#775566")).imageWithRenderingMode(.AlwaysOriginal) //#ee9977
                 }
-                item.selectedImage = finalImage.imageWithColor(UIColor(rgba: "#7F8C8D")).imageWithRenderingMode(.AlwaysOriginal) //#ee9977
             }
             i++
         }
