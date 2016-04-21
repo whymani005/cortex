90f42ca49b5ebbff7625ca2e2e0d6d7d9bae309f
Manisha Yeramareddy <manisha.yeramareddy@cashcall.com>
2016-04-21 01:17:50 -0700

    remove NSLog

--- Cortex/AppDelegate.swift
+++ Cortex/AppDelegate.swift
@@ -25,13 +25,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // change navigation item title color
         navigationBarAppearace.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.whiteColor()]
                 
-        let defaults = NSUserDefaults.standardUserDefaults()
+        /*let defaults = NSUserDefaults.standardUserDefaults()
         let isPreloaded = defaults.boolForKey("isPreloaded")
         if !isPreloaded {
             print("BEFORE preloadQuotes")
             //self.preloadQuotes()
             defaults.setBool(true, forKey: "isPreloaded")
-        }
+        }*/
         
         return true
     }
@@ -95,7 +95,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
             // Replace this with code to handle the error appropriately.
             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(error), \(error!.userInfo)")
+            print("Unresolved error \(error), \(error!.userInfo)")
             abort()
         } catch {
             fatalError()
@@ -128,7 +128,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     error = error1
                     // Replace this implementation with code to handle the error appropriately.
                     // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    NSLog("Unresolved error \(error), \(error!.userInfo)")
+                    print("Unresolved error \(error), \(error!.userInfo)")
                     abort()
                 }
             }
